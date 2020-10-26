41a7ee3e2aa57df54c64f8af91c5aa5a9bcc0098
eathanq <644387875@qq.com>
2020-10-26 07:03:32 -0700

    Enhance SmartPointer Code

--- SmartPointer.c
+++ SmartPointer.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define autofree __attribute__((cleanup(free_stack)))
+
+void sfree(void *ptr);
 
 __attribute__ ((always_inline))
 inline void free_stack(void *p)
 {
-	free(*(void **) p);
+	sfree(*(void **) p);
+}
+
+struct meta{
+	void (*dtor)(void *);
+	void *ptr;
+};
+
+__attribute__ ((malloc))
+void *smalloc(size_t size, void(*dtor)(void *))
+{
+	void * p = malloc(sizeof(struct meta) + size);
+	struct meta * m = (struct meta *)p;
+	m->dtor = dtor;
+	m->ptr = m + 1;
+	return m->ptr;	
+}
+
+void sfree(void *ptr)
+{
+	void *tmp = ptr - sizeof(struct meta);
+	struct meta *m = (struct meta *)tmp;
+	assert(m->ptr == ptr);
+	if (m->dtor != NULL) 
+		m->dtor(ptr);
+	free(m);
 }
 
 int main()
 {
-	autofree int *i = malloc(sizeof(int) * 10);
+	autofree int *i = smalloc(sizeof(int) * 10, NULL);
 	i[0] = 0;
 	i[1] = 1;
 	printf("SmartPointer Easy Test:%d\n", i[1]);
